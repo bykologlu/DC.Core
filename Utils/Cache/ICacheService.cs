51b3f2b206cd080c539fa2a71118e3ff213428f7
Mustafa Koloğlu <bykologlu@hotmail.com>
2023-12-28 20:56:42 +0300

    cache service fixed

--- Utils/Cache/ICacheService.cs
+++ Utils/Cache/ICacheService.cs
@@ -1,6 +1,14 @@
 ﻿namespace DC.Core.Utils.Cache
 {
-    public  interface ICacheService
+    public interface ICacheService
     {
+        Task<T> GetValueAsync<T>(string key);
+        Task<bool> SetValueAsync(string key, string value, int? duration = null);
+        Task<T> GetOrAddAsync<T>(string key, Func<T> action) where T : class;
+        Task ClearAsync(string key);
+        Task ClearAllASync();
+        Task<bool> Lock(string key, string value, int? duration = null);
+        Task<bool> UnLock(string key, string value);
+        Task GetLock(string key, string value, Func<Task> action);
     }
 }
